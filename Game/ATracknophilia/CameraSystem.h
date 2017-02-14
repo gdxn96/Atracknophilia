5acf68d733667325451278bb1d157deb47ae3ad1
GavinOB1994 <gavinob1994@gmail.com>
2017-02-14 09:30:21 +0000

    GJPTEAME-21 #time 0h 50m #comment commited so i can pull in master

--- Game/ATracknophilia/CameraSystem.h
+++ Game/ATracknophilia/CameraSystem.h
@@ -11,11 +11,13 @@ public:
 	}
 	void process(float dt) override
 	{
-		auto ents = AutoList::get<Player>();
-
+		for (auto& component : AutoList::get<Player>())
+		{
+			m_cam->setCentre(component->)
+		}
 	}
 
 private:
-
+	Camera2D::Camera * m_cam;
 };
 
