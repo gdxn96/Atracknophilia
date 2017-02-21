7a6b0cd72b877d35aa7c0e03a1a819116b00861f
Robbie <wavchild@gmail.com>
2017-02-21 10:52:28 +0000

    GJPTEAME-35 #time 10m started swap shot

--- Game/ATracknophilia/Interactables.h
+++ Game/ATracknophilia/Interactables.h
@@ -47,3 +47,36 @@ struct HookComponent : public IComponent, public AutoLister<HookComponent>
 	LineComponent* line;
 	float tetherLength;
 };
+
+
+struct SwapComponent : public IComponent, public AutoLister<SwapComponent>
+{
+	SwapComponent(int id, Vector2D start, Vector2D end, b2Body* bodyToAttach)
+		: IComponent(id)
+		//, pivot(new StaticBodyComponent(-1, end.x, end.y, 0, 0, true))
+		, tetherLength(Vector2D::Distance(start, end) * 0.9f)
+		, line(new LineComponent(-1, start, end))
+	{
+		b2DistanceJointDef jointDef;
+		jointDef.bodyA = bodyToAttach;
+		//jointDef.bodyB = pivot->body;
+		jointDef.collideConnected = false;
+		jointDef.length = tetherLength;
+		jointDef.dampingRatio = 0.5;
+		jointDef.frequencyHz = 60;
+
+		joint = (b2DistanceJoint*)PhysicsSystem::World().CreateJoint(&jointDef);
+	}
+
+	virtual ~SwapComponent()
+	{
+		PhysicsSystem::World().DestroyJoint(joint);
+		//delete pivot;
+		delete line;
+	}
+
+	b2DistanceJoint* joint;
+	//Box2DComponent* pivot;
+	LineComponent* line;
+	float tetherLength;
+};
