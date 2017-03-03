29b39cb073e11979010d0f48e2a3e568879f1863
Sean Byrne <seanbyrne999@gmail.com>
2017-03-03 14:37:20 +0000

    Adding options menu

--- Game/ATracknophilia/OptionsScene.cpp
+++ Game/ATracknophilia/OptionsScene.cpp
@@ -4,17 +4,33 @@
 OptionsScene::OptionsScene(Vector2D windowSize)
 	: Scene(Scenes::OPTIONS)
 	, m_textureRect(0, 0, windowSize.x, windowSize.y)
+	, m_leftBtnPos(200)
+	, m_rightBtnPos(800)
+	, m_yBtnPos(350)
+	, m_btnHeight(50)
+	, m_btnWidth(250)
 {
+	m_onBtn = Button();
+	m_offBtn = Button();
+	m_highlightedBtn = Button();
+	m_displayBtn = Button();
+	m_selectedBtn = Button();
 	loadMedia();
 }
 
 void OptionsScene::update(float dt)
 {
+	currentTick += dt * 1000; // dt was rounding down to 0 when adding 0.0001 of a second so converting into milliseonds
 }
 
 void OptionsScene::render(Renderer & r)
 {
 	r.drawTexture(m_splashScreen, m_textureRect);
+	m_onBtn.render(r);
+	m_offBtn.render(r);
+	m_displayBtn.render(r);
+	m_selectedBtn.render(r);
+	m_highlightedBtn.render(r);
 	r.present();
 }
 
@@ -23,8 +39,27 @@ bool OptionsScene::init(Renderer & r)
 	// init success flag
 	bool success = true;
 
+	m_onBtn.setRect(Rect{ m_leftBtnPos, m_yBtnPos, m_btnWidth, m_btnHeight });
+	m_offBtn.setRect(Rect{ m_rightBtnPos, m_yBtnPos, m_btnWidth, m_btnHeight });
+	m_displayBtn.setRect(Rect{ m_rightBtnPos - 350, m_yBtnPos +100, 350, 70 });
+	m_selectedBtn.setRect(Rect{ m_rightBtnPos, m_yBtnPos +110, m_btnWidth, m_btnHeight });
+	m_highlightedBtn.setRect(Rect{ m_leftBtnPos, m_yBtnPos, m_btnWidth, m_btnHeight });
+
+	m_onBtn.setTexture(ResourceManager::getInstance()->getTextureByKey("onbtn"));
+	m_offBtn.setTexture(ResourceManager::getInstance()->getTextureByKey("offbtn"));
+	m_displayBtn.setTexture(ResourceManager::getInstance()->getTextureByKey("soundeffectsbtn"));
+	m_selectedBtn.setTexture(ResourceManager::getInstance()->getTextureByKey("onbtn"));
+	m_highlightedBtn.setTexture(ResourceManager::getInstance()->getTextureByKey("highlight"));
+
+	m_highlightedBtn.setDirection(left);
+
 	// set key binds
 	InputManager::GetInstance()->RegisterEventCallback(EventListener::BUTTON_B, new PressCommand(std::bind(&OptionsScene::changeScene, this, Scenes::SPLASH)), this, 0);
+	// player 1
+	InputManager::GetInstance()->RegisterEventCallback(EventListener::BUTTON_DPAD_LEFT, new PressCommand(std::bind(&OptionsScene::moveHighlightBtn, this, left, m_playerOneID)), this, m_playerOneID);
+	InputManager::GetInstance()->RegisterEventCallback(EventListener::BUTTON_DPAD_RIGHT, new PressCommand(std::bind(&OptionsScene::moveHighlightBtn, this, right, m_playerOneID)), this, m_playerOneID);
+	InputManager::GetInstance()->RegisterEventCallback(EventListener::BUTTON_START, new PressCommand(std::bind(&OptionsScene::executeScene, this)), this, m_playerOneID);
+	InputManager::GetInstance()->RegisterEventCallback(EventListener::BUTTON_A, new PressCommand(std::bind(&OptionsScene::executeScene, this)), this, m_playerOneID);
 
 	return success;
 }
@@ -32,7 +67,11 @@ bool OptionsScene::init(Renderer & r)
 void OptionsScene::changeScene(Scenes newScene)
 {
 	if (SceneManager::getInstance()->getCurrentScene()->getTitle() == Scenes::OPTIONS)
+	{
+		currentTick = 0;
 		SceneManager::getInstance()->switchTo(newScene);
+	}
+	
 }
 
 bool OptionsScene::loadMedia()
@@ -44,4 +83,38 @@ bool OptionsScene::loadMedia()
 	m_splashScreen = ResourceManager::getInstance()->getTextureByKey("optionsscreen");
 
 	return success;
+}
+
+void OptionsScene::moveHighlightBtn(direction dir, IDs id)
+{
+	if (id == m_playerOneID)
+	{
+		if (dir == left && m_highlightedBtn.getDirection() != left)
+		{
+			m_highlightedBtn.setDirection(left);
+			m_highlightedBtn.setRect(Rect{ m_leftBtnPos, m_yBtnPos, m_btnWidth, m_btnHeight });
+		}
+		if (dir == right && m_highlightedBtn.getDirection() != right)
+		{
+			m_highlightedBtn.setDirection(right);
+			m_highlightedBtn.setRect(Rect{ m_rightBtnPos, m_yBtnPos, m_btnWidth, m_btnHeight });
+		}
+	}
+}
+
+void OptionsScene::executeScene()
+{
+	if (currentTick > 1)
+	{
+		if (m_onBtn.getRect() == m_highlightedBtn.getRect())
+		{
+			m_selectedBtn.setTexture(ResourceManager::getInstance()->getTextureByKey("onbtn"));
+			//changeScene(Scenes::CHOOSEPLAYER);
+		}
+		if (m_offBtn.getRect() == m_highlightedBtn.getRect())
+		{
+			m_selectedBtn.setTexture(ResourceManager::getInstance()->getTextureByKey("offbtn"));
+			//changeScene(Scenes::ROOMSCENE);
+		}
+	}
 }
--- Game/ATracknophilia/OptionsScene.h
+++ Game/ATracknophilia/OptionsScene.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "SceneManager.h"
 #include "Game.h"
+#include "Button.h"
 
 class OptionsScene : public Scene, public EventListener
 {
@@ -16,9 +17,31 @@ public:
 private:
 
 	bool loadMedia();
-
 	//The image we will load and show on the screen
 	SDL_Texture* m_splashScreen = NULL;
 	Rect m_textureRect;
 	int currentTick = 0;
+
+	Button m_onBtn;
+	Button m_offBtn;
+
+	Button m_displayBtn, m_selectedBtn;
+
+	Button m_highlightedBtn;
+
+	// button positions on the screen
+	float m_leftBtnPos, m_rightBtnPos, m_yBtnPos;
+
+	// buttons height and weight
+	float m_btnWidth, m_btnHeight;
+
+	// controller ID's used to determine player number
+	enum IDs { m_playerOneID, m_playerTwoID, m_playerThreeID, m_playerFourID };
+
+	// used for changing character by left or right ordering
+	enum direction { left, right };
+
+	void moveHighlightBtn(direction dir, IDs id);
+
+	void executeScene();
 };
